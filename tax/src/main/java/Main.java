e796e16b9161f8165ca62521e1868a546ddff378
Vladimir Golubnichiy <v.golubnichiy@logitex.kz>
2017-12-20 08:31:52 +0600

    Init commit

---
+++ tax/src/main/java/Main.java
@@ -0,0 +1,41 @@
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        File myFile = new File("G://test.xlsx");
+        FileInputStream fis = new FileInputStream(myFile);
+
+        // Finds the workbook instance for XLSX file
+        XSSFWorkbook myWorkBook = new XSSFWorkbook(fis);
+
+        // Return first sheet from the XLSX workbook
+        XSSFSheet mySheet = myWorkBook.getSheetAt(0);
+
+        // Traversing over each row of XLSX file
+        for (Row row : mySheet) {
+            for (Cell cell : row) {
+                switch (cell.getCellType()) {
+                    case Cell.CELL_TYPE_STRING:
+                        System.out.print(cell.getStringCellValue() + "\t");
+                        break;
+                    case Cell.CELL_TYPE_NUMERIC:
+                        System.out.print(cell.getNumericCellValue() + "\t");
+                        break;
+                    case Cell.CELL_TYPE_BOOLEAN:
+                        System.out.print(cell.getBooleanCellValue() + "\t");
+                        break;
+                    default:
+
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
